a83a51bbfbc50c587a07f0dc39edfe82ce603f18
Chloe Ransom <c.ransom12@hotmail.co.uk>
2022-11-11 11:40:07 +0100

    Add stupid budget test

--- budget/tests.py
+++ budget/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
+from budget.models import Budget
+
 # Create your tests here.
+class ModelTests(TestCase):
+    def test_budget_creation(self):
+        """Budget can be created with id"""
+        budget = Budget(1)
+        self.assertTrue(isinstance(budget, Budget))
